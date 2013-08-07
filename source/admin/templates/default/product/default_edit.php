ae09dacf22deeb8ae1cbed7c0b1c3978ee51fd11
Manish <manish@readybytes.in>
2013-08-07 17:39:37 +0530

    Move jquery code into anonymous function(in Product edit tmpl). Fixed into alias creation and break code for category creation by ajax

--- source/admin/templates/default/product/default_edit.php
+++ source/admin/templates/default/product/default_edit.php
@@ -27,58 +27,84 @@ PaycartHtml::_('behavior.formvalidation');
 	(function($){
 
 		// Change Attributes on bases of product type		
-		typeAttributes = function(type) {
-				switch(type) 
-				{//@PCTODO :: use constant
-					case '10':	// Physical type
-						$('.paycart_product_digital_file').hide();
-						$('.paycart_product_quantity').show();
-						break;
-					case '20' :	// Digital Type
-						$('.paycart_product_digital_file').show();
-						$('.paycart_product_quantity').hide();
-						break;
+		typeAttributes = function(type) 
+		{
+			switch(type) 
+			{//@PCTODO :: use constant
+				case '10':	// Physical type
+					$('.paycart_product_digital_file').hide();
+					$('.paycart_product_quantity').show();
+					break;
+				case '20' :	// Digital Type
+					$('.paycart_product_digital_file').show();
+					$('.paycart_product_quantity').hide();
+					break;
+			}
+
+		};	
+
+		$(document).ready(function($){
+			
+			typeAttributes($('#paycart_form_type').val());
+
+			$('#paycart_form_type').change( function() {
+				typeAttributes($(this).val());
+			});
+
+			<!-- Callback function when Alias successfully generated				-->
+			var callbackOnSuccess = function(data)
+			{	// Add alias to element
+				$('#paycart_form_alias').val(data[0][1]);
+			};
+			
+			// When product title assign then create alias			
+			$('#paycart_form_title').blur( function() 
+			{
+					var title = $(this).val();
+			
+					// if title empty or alias pre-define
+					if (!title || $('#paycart_form_alias').val()) {
+						return true;
+					}
+
+					// Get Product id
+					var id = $('input[name="id"]').val();
+					
+					// pass title, Product ID, callbackOnSuccess,  callbackOnError
+					//@PCTODO :: Proper Error-handling in callbackOnError
+					paycart.alias.add(	title, id,callbackOnSuccess,
+										function(){alert('error in alias generating')}
+							);
+			});
+
+			// When alias is empty then create new alias
+			$('#paycart_form_alias').blur( function() 
+			{
+				var alias = $(this).val();
+				var title = $('#paycart_form_title').val();
+
+				// if alias pre-define Or product title empty 
+				if (alias || !title) {
+					return true;
 				}
-			};	 	
+
+				// Get Product id
+				var id = $('input[name="id"]').val();
+
+				// pass title, Product ID , callbackOnSuccess,  callbackOnError
+				//@PCTODO :: Proper Error-handling in callbackOnError
+				paycart.alias.add(	title, id,callbackOnSuccess,
+									function(){alert('error in alias generating')}
+						);
+			});
+			
+		});
+			
+	 	
 	})(paycart.jQuery);
 	
 
-	paycart.jQuery(document).ready(function($){
-		<!-- Callback function when Alias successfully generated				-->
-		var callbackOnSuccess = function(data)
-		{	// Add alias to element
-			$('#paycart_form_alias').val(data[0][1]);
-		};
-		
-		<!-- Callback function when error occur during category adding operation	-->
-		var callbackOnError = function ()
-		{
-			//@PCTODO :: Proper Error-handling 
-			alert('error in alias generating');
-		};
 	
-		$('#paycart_form_title').blur( function()
-		{
-			var title = $(this).val();
-	
-			// if title empty or alias pre-define
-			if (!title || $('#paycart_form_alias').val()) {
-				return true;
-			}
-	
-			paycart.alias.add(title, callbackOnSuccess, callbackOnError);
-			
-		});
-
-		typeAttributes($('#paycart_form_type').val());
-
-		$('#paycart_form_type').change(
-						function() {
-							typeAttributes($(this).val());
-						});
-		
-		
-	});
 </script>
 
 <form action="<?php echo $uri; ?>" method="post" name="adminForm" id="adminForm" class="rb-validate-form" enctype="multipart/form-data" >
