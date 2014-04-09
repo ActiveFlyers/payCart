cb8b8d0518eee88fabf27be4a040d080ece478ee
Shyam Sunder Verma <shyam@readybyts.in>
2014-04-09 18:49:06 +0530

    Product display html & css added 1. Product have whitespace border 2. on-hover highlight blue border 3. Added secondary content html

--- source/site/templates/default/catlogue/products.php
+++ source/site/templates/default/catlogue/products.php
@@ -14,24 +14,31 @@ if(!defined( '_JEXEC' )){
 }?>
 
 <?php
-
 $catlogue = include JPATH_ROOT.'/test/_data/ux/catlogue1.php';
 
 ?>
 
-<div class='pc-products-wrapper row-fluid clearfix'>
-<div id="pc-products" class='pc-products span12 clearfix' data-columns>
-	<?php foreach($catlogue->product as $p) : ?>
-	<div class="pc-product-outer thumbnails">
-		<div class='pc-product '>
-				<div class="pc-product-content">
+<div class='pc-products-wrapper row-fluid clearfix'> 
+	<div id="pc-products" class ='pc-products span12 clearfix' data-columns>     
+	<?php foreach($catlogue->product as $p) : ?>     
+		<div class="pc-product-outer ">
+			<div class='pc-product thumbnail'>                 
+				<div class="pc-product-content">                    
 					<img src="<?php echo $p['cover_media'];?>">
-					<h3><a href="#"><?php echo $p['title'];?></a></h3>
+					<h3><a href="#"><?php echo $p['title'];?></a></h3>                    
+					<h4><span class="currency">$</span> <span class="amount">210.00</span></h4>
+				
+					<div class="pc-separator">&nbsp;</div>
+					
+					<h4 class="teaser row-fluid muted">
+						<span class="pull-left"><del>300</del></span>
+						<span class="text-right pull-right">30% OFF</span>
+					</h4>					
 				</div>
+			</div>
 		</div>
-	</div>
 	<?php endforeach;?>
-</div>
+	</div>
 </div>
 <?php
 
