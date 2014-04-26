2715349742f58d51646fdf58e3d4a1652ce87fe5
Shyam Sunder Verma <shyam@readybyts.in>
2014-04-26 11:56:25 +0530

    style: formats js variables

--- source/media/js/paycart.js
+++ source/media/js/paycart.js
@@ -8,9 +8,9 @@
 
 if (typeof(paycart)=='undefined'){
 	var paycart 	= {};
-	paycart.$ 	= paycart.jQuery = rb.jQuery;
+	paycart.$ 		= paycart.jQuery = rb.jQuery;
 	paycart.ajax	= rb.ajax;
-	paycart.ui	= rb.ui;
+	paycart.ui		= rb.ui;
 	paycart.queue	= new Array(); 
 }
 
