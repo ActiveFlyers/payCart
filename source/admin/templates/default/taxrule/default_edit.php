6fbecb9faf2593b409a3a30fa8ceec40d7995775
Gaurav Jain <gaurav@readybytes.in>
2014-07-15 14:05:36 +0530

    task::Creates admin screens for groups, tax rules and discount rules

---
+++ source/admin/templates/default/taxrule/default_edit.php
@@ -0,0 +1,186 @@
+<?php
+/**
+* @copyright	Copyright (C) 2009 - 2013 Ready Bytes Software Labs Pvt. Ltd. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* @package 		PAYCART
+* @subpackage	Back-end
+* @contact		support+paycart@readybytes.in 
+*/
+
+defined('_JEXEC') or die( 'Restricted access' );
+?>
+<script>
+paycart.admin.taxrule = {};
+
+(function($){
+	paycart.admin.taxrule.getProcessorConfig = function(){
+		var processor_classname = $('[data-pc-taxrule="processor"] select').val();
+		if(processor_classname.length > 0){
+			var url  = 'index.php?option=com_paycart&view=taxrule&task=getProcessorConfig&processor_classname='+processor_classname+'&taxrule_id='+<?php echo $form->getValue('taxrule_id');?>;
+			paycart.ajax.go(url);
+		}
+		else{
+			$('#pc-taxrule-processorconfig').html('');
+		}	
+	};
+
+	$(document).ready(function(){
+		paycart.admin.taxrule.getProcessorConfig();
+		$('[data-pc-taxrule="processor"] select').change(function(){					
+			paycart.admin.taxrule.getProcessorConfig();
+		});
+	});
+})(paycart.jQuery);
+</script>
+
+<div class="pc-product-wrapper clearfix">
+<div class="pc-taxrule row-fluid">
+
+<!-- CONTENT START -->
+
+<!-- ADMIN MENU -->
+<div class="span2">
+	<?php
+			$helper = PaycartFactory::getHelper('adminmenu');			
+			echo $helper->render('index.php?option=com_paycart&view=taxrule'); 
+	?>
+</div>
+<!-- ADMIN MENU -->
+
+
+<div class="span10">
+<div class="row-fluid">	
+	<form action="<?php echo $uri; ?>" method="post" name="adminForm" id="adminForm" class="rb-validate-form">
+		<div class="row-fluid">
+			<div class="span3">
+				<h2><?php echo JText::_('COM_PAYCART_ADMIN_TAXRULE_DETAILS_HEADER');?></h2>
+				<div>
+					<?php echo JText::_('COM_PAYCART_ADMIN_TAXRULE_DETAILS_HEADER_MSG');?>
+				</div>
+			</div>
+			<div class="span9">
+				<fieldset class="form">
+					<div class="row-fluid">
+						<?php $field = $form->getField('title') ?>
+						<div class="control-group">
+							<div class="control-label"><?php echo $field->label; ?> </div>
+							<div class="controls"><?php echo $field->input; ?></div>								
+						</div>
+						<?php $field = $form->getField('description') ?>
+						<div class="control-group">
+							<div class="control-label"><?php echo $field->label; ?> </div>
+							<div class="controls"><?php echo $field->input; ?></div>								
+						</div>
+						<?php $field = $form->getField('message') ?>
+						<div class="control-group">
+							<div class="control-label"><?php echo $field->label; ?> </div>
+							<div class="controls"><?php echo $field->input; ?></div>								
+						</div>
+					</div>
+					<div class="row-fluid">
+						<div class="span6">
+							<?php $field = $form->getField('published') ?>
+							<div class="control-group">
+								<div class="control-label"><?php echo $field->label; ?> </div>
+								<div class="controls"><?php echo $field->input; ?></div>								
+							</div>
+						</div>
+						<div class="span6">							
+						</div>
+					</div>
+				</fieldset>
+			</div>
+		</div>		
+		
+		<hr/>
+		
+		<div class="row-fluid">
+			<div class="span3">
+				<h2><?php echo JText::_('COM_PAYCART_ADMIN_TAXRULE_GROUPS_HEADER');?></h2>
+				<div>
+					<?php echo JText::_('COM_PAYCART_ADMIN_TAXRULE_GROUPS_HEADER_MSG');?>
+				</div>
+			</div>
+			<div class="span9">
+				<div class="row-fluid">				
+					<div class="control-group">
+						<?php $field = $form->getField('_buyergroups') ?>
+						<div class="control-label"><?php echo JText::_('COM_PAYCART_ADMIN_GROUPRULE_TYPE_BUYER_SELECTION_MSG'); ?></div>
+						<div class="controls"><?php echo $field->input; ?></div>	
+					</div>
+				</div>
+				<br/>
+				<div class="row-fluid"><?php echo JText::_('COM_PAYCART_ADMIN_AND');?></div>
+				<br/>
+				<div class="row-fluid">
+					<div class="control-group">
+						<?php $field = $form->getField('_productgroups') ?>
+						<div class="control-label"><?php echo JText::_('COM_PAYCART_ADMIN_GROUPRULE_TYPE_PRODUCT_SELECTION_MSG'); ?></div>
+						<div class="controls"><?php echo $field->input; ?></div>
+					</div>
+				</div>
+				<br/>
+				<div class="row-fluid"><?php echo JText::_('COM_PAYCART_ADMIN_AND');?></div>
+				<br/>
+				<div class="row-fluid">
+					<div class="control-group">
+						<?php $field = $form->getField('_cartgroups') ?>
+						<div class="control-label"><?php echo JText::_('COM_PAYCART_ADMIN_GROUPRULE_TYPE_CART_SELECTION_MSG'); ?></div>
+						<div class="controls"><?php echo $field->input; ?></div>
+					</div>
+				</div>			
+			</div>
+		</div>
+		
+		<hr />
+		
+		<div class="row-fluid">
+			<div class="span3">
+				<h2><?php echo JText::_('COM_PAYCART_ADMIN_TAXRULE_CONFIG_HEADER');?></h2>
+				<div>
+					<?php echo JText::_('COM_PAYCART_ADMIN_TAXRULE_CONFIG_HEADER_MSG');?>
+				</div>
+			</div>
+			<div class="span9">
+				<div class="row-fluid">
+					<div><?php echo Jtext::_('COM_PAYCART_ADMIN_TAXRULE_APPLY');?></div>
+					<?php $field = $form->getField('amount') ?>
+					<div class="control-group">						
+						<div class="controls"><?php echo $field->input; ?></div>								
+					</div>
+				</div>
+				<br/>		
+				<div class="row-fluid">		
+					<?php $field = $form->getField('apply_on') ?>
+					<div><?php echo Jtext::_('COM_PAYCART_ADMIN_TAXRULE_ON');?></div>
+					<div class="control-group">						
+						<div class="controls"><?php echo $field->input; ?></div>								
+					</div>
+				</div>				
+				<br/>
+				<div class="row-fluid">					
+					<div><?php echo Jtext::_('COM_PAYCART_ADMIN_TAXRULE_BY');?></div>
+					<div class="control-group">
+						<div class="controls" data-pc-taxrule="processor">
+							<?php $field = $form->getField('processor_classname') ?>
+							<?php echo $field->input; ?>
+						</div>	
+					</div>
+					
+					<fieldset class="form-horizontal">
+						<div id="pc-taxrule-processorconfig">
+							&nbsp;
+						</div>
+					</fieldset>
+				</div>
+			</div>
+		</div>
+		<?php echo $form->getInput('taxrule_id'); ?>
+		<input type="hidden" name="task" value="" />
+	</form>
+</div>
+</div>
+</div>
+</div>
+<?php 
+	
---
+++ source/admin/views/discountrule/view.ajax.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+* @copyright	Copyright (C) 2009 - 2013 Ready Bytes Software Labs Pvt. Ltd. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* @package 		PAYCART
+* @subpackage	Back-end
+* @contact		support+paycart@readybytes.in 
+*/
+
+defined('_JEXEC') or die( 'Restricted access' );
+
+/**
+ * Admin Ajax View for Discount Rules
+ * 
+ * @since 1.0.0
+ */
+require_once dirname(__FILE__).'/view.php';
+class PaycartAdminViewDiscountrule extends PaycartAdminBaseViewTaxrule
+{
+	public function getProcessorConfig()
+	{
+		$discountrule_id = $this->getModel()->getId();
+		$data 			= array();
+		$data['processor_classname']	=  $this->input->get('processor_classname', '');
+		
+		$discountrule 		= PaycartDiscountrule::getInstance($discountrule_id)->bind($data);		
+		$html = $discountrule->getProcessorConfigHtml();
+		$this->assign('processor_config_html', $html);
+		$this->setTpl('processor_config');
+		$this->_renderOptions = array('domObject'=>'pc-discountrule-processorconfig','domProperty'=>'innerHTML');
+		return true;
+	}
+}
