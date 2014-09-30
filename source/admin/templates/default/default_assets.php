b5ae71d51ad7c11e09b6e1622e717d49fcca466e
rbsl-rimjhim <rimjhim@readybytes.in>
2014-09-30 14:59:38 +0530

    chore::revert the way to load script and stylesheet in default_assets.php
    We will fix it later

--- source/admin/templates/default/default_assets.php
+++ source/admin/templates/default/default_assets.php
@@ -20,11 +20,11 @@ Rb_HelperTemplate::loadMedia(array('jquery', 'bootstrap', 'rb', 'font-awesome'))
 // load bootsrap css
 Rb_Html::_('bootstrap.loadcss');
 
-Rb_Html::script('com_paycart/paycart.js');
-Rb_Html::script('com_paycart/validate.js');
+Rb_Html::script(PAYCART_PATH_CORE_MEDIA.'/js/paycart.js');
+Rb_Html::script(PAYCART_PATH_CORE_MEDIA.'/js/validate.js');
 Rb_Html::script(dirname(__FILE__).'/_media/js/admin.js');
 
-Rb_Html::stylesheet('com_paycart/paycart.css');
+Rb_Html::stylesheet(PAYCART_PATH_CORE_MEDIA.'/css/paycart.css');
 Rb_Html::stylesheet(dirname(__FILE__).'/_media/css/admin.css');
 
 Rb_Html::_('formbehavior.chosen', 'select');
