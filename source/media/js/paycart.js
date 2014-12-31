5cb396b42472a06f44dc8f7dafbc93a3b96cfaae
Gaurav Jain <gaurav@readybytes.in>
2014-12-31 18:04:41 +0530

    improve :: Maintains ab state after redirection also
    ref #421

--- source/media/js/paycart.js
+++ source/media/js/paycart.js
@@ -219,3 +219,49 @@ if (typeof(paycart.element)=='undefined'){
 // Scoping code for easy and non-conflicting access to $.
 // Should be last line, write code above this line.
 })(paycart.jQuery);
+
+
+
+// Maintain Tab State
+(function($){
+	
+    var loadTab = function() {
+
+        $('.paycart a[data-toggle="tab"]').on('click', function(e) {
+            // Store the selected tab href in localstorage
+            window.localStorage.setItem('tab-href', $(this).attr('href'));
+        });
+
+        var activateTab = function(href) {
+            var $el = $('.paycart a[data-toggle="tab"]a[href*=' + href + ']');
+            $el.tab('show');
+        };
+
+        var hasTab = function(href){
+            return $('.paycart a[data-toggle="tab"]a[href*=' + href + ']').length;
+        };
+
+        $(document).ready(function(){
+        if (localStorage.getItem('tab-href') && localStorage.getItem('tab-href') !== 'undefined') {
+            // When moving from tab area to a different view
+            if(!hasTab(localStorage.getItem('tab-href'))){
+                localStorage.removeItem('tab-href');
+                return true;
+            }
+            // Clean default tabs
+            $('.paycart a[data-toggle="tab"]').parent().removeClass('active');
+            var tabhref = localStorage.getItem('tab-href');
+            
+            // Add active attribute for selected tab indicated by url
+            activateTab(tabhref);
+            // Check whether internal tab is selected (in format <tabname>-<id>)
+            var seperatorIndex = tabhref.indexOf('-');
+            if (seperatorIndex !== -1) {
+                var singular = tabhref.substring(0, seperatorIndex);
+                var plural = singular + "s";
+                activateTab(plural);
+            }
+        }});
+    };
+    setTimeout(loadTab, 500);
+})(paycart.jQuery);
