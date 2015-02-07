6ada0f3c61d8e69da1b3b3b9deacca6350388a16
Gaurav Jain <gaurav@readybytes.in>
2015-02-07 17:08:37 +0530

    chore :: Adds upgrade methon in modules and apply caching of loading style

--- modules/mod_paycart_product/tmpl/default.php
+++ modules/mod_paycart_product/tmpl/default.php
@@ -19,93 +19,106 @@
 // no direct access
 defined('_JEXEC') or die;
 ?>
-
-<?php static $pc_mod_product_loaded = false; ?>
-<?php if($pc_mod_product_loaded == false) :?>
-	<style>
-	
-	.pc-module-product{
-		position: relative;	
-	}
-	
-	.pc-mod-products{	
-		padding: 20px 0px;
-	}
-	
-	.pc-mod-products .pc-mod-product{    
-	    margin:10px;
-	    position: relative;    
-	}
-	
-	.pc-mod-products .pc-mod-product .pc-mod-product-img{    
-	    overflow: hidden;
-	    max-height: 300px;
-	}  
-	  
-	.pc-mod-products .pc-mod-product .pc-mod-product-img img {
-	    width : 100%;
-	    -webkit-transition: all 300ms ease-out;  
-	    -moz-transition: all 300ms ease-out;  
-	    -o-transition: all 300ms ease-out;  
-	    -ms-transition: all 300ms ease-out;  
-	    transition: all 300ms ease-out;      
-	} 
-	
-	.pc-mod-products .pc-mod-product .pc-mod-product-view{
-		position: absolute;
-		/*background-color: rgba(0, 0, 0, 0.8);*/
-	    color: #FFFFFF;
-		z-index: 100;
-		left: 0px;	
-		right: 0px;
-		top: 0px;
-		bottom:0px;
-		-webkit-transition: all 300ms ease-out 0s;  
-	    -moz-transition: all 300ms ease-out 0s;  
-	    -o-transition: all 300ms ease-out 0s;  
-	    -ms-transition: all 300ms ease-out 0s;  
-		transition: all 300ms ease-out 0s;	
-		text-align:center;
-		opacity: 0;
-	}
-	
-	.pc-mod-products .pc-mod-product .pc-mod-product-view span{
-		position: absolute;
-		z-index: 101;
-		top: 50%;
-		left: 50%;
-		
-		transform: translate(-50%, -50%);
-		-moz-transform: translate(-50%, -50%);  
-	    -o-transform: translate(-50%, -50%);  
-	    -webkit-transform: translate(-50%, -50%);
-	}
-	
-		  
-	.pc-mod-products .pc-mod-product:hover .pc-mod-product-view{    
-	    opacity: 1;    
-	}
-	
-	.pc-mod-products .pc-mod-product:hover img {  
-	       -moz-transform: scale(1.2);  
-	       -o-transform: scale(1.2);  
-	       -webkit-transform: scale(1.2);  
-	       transform: scale(1.2);  
-	}
-	  
-	.pc-mod-ellipsis{
-		text-overflow: ellipsis;
-		max-width:98%;
-		overflow:hidden;
-	}
-	</style>
-	<?php $pc_mod_product_loaded = true;?>
-<?php endif;?>
+<?php if(!function_exists('pc_mod_product_style')) :?>
+	<?php function pc_mod_product_style(){?>
+		<?php static $pc_mod_product_loaded = false; ?>
+			<?php if($pc_mod_product_loaded == false) :?>
+				<style>
+				
+				.pc-module-product{
+					position: relative;	
+					margin : 0px 50px;
+				}
+				
+				.pc-mod-products{	
+					padding: 20px 0px;
+				}
+				
+				.pc-mod-products .pc-mod-product{    
+				    margin:10px;
+				    position: relative;    
+				}
+				
+				.pc-mod-products .pc-mod-product .pc-mod-product-img{    
+				    overflow: hidden;
+				    max-height: 300px;
+				}  
+				  
+				.pc-mod-products .pc-mod-product .pc-mod-product-img img {
+				    width : 100%;
+				    -webkit-transition: all 300ms ease-out;  
+				    -moz-transition: all 300ms ease-out;  
+				    -o-transition: all 300ms ease-out;  
+				    -ms-transition: all 300ms ease-out;  
+				    transition: all 300ms ease-out;      
+				} 
+				
+				.pc-mod-products .pc-mod-product .pc-mod-product-view{
+					position: absolute;
+					/*background-color: rgba(0, 0, 0, 0.8);*/
+				    color: #FFFFFF;
+					z-index: 100;
+					left: 0px;	
+					right: 0px;
+					top: 0px;
+					bottom:0px;
+					-webkit-transition: all 300ms ease-out 0s;  
+				    -moz-transition: all 300ms ease-out 0s;  
+				    -o-transition: all 300ms ease-out 0s;  
+				    -ms-transition: all 300ms ease-out 0s;  
+					transition: all 300ms ease-out 0s;	
+					text-align:center;
+					opacity: 0;
+				}
+				
+				.pc-mod-products .pc-mod-product .pc-mod-product-view span{
+					position: absolute;
+					z-index: 101;
+					top: 50%;
+					left: 50%;
+					
+					transform: translate(-50%, -50%);
+					-moz-transform: translate(-50%, -50%);  
+				    -o-transform: translate(-50%, -50%);  
+				    -webkit-transform: translate(-50%, -50%);
+				}
+				
+					  
+				.pc-mod-products .pc-mod-product:hover .pc-mod-product-view{    
+				    opacity: 1;    
+				}
+				
+				.pc-mod-products .pc-mod-product:hover img {  
+				       -moz-transform: scale(1.2);  
+				       -o-transform: scale(1.2);  
+				       -webkit-transform: scale(1.2);  
+				       transform: scale(1.2);  
+				}
+				  
+				.pc-mod-ellipsis{
+					text-overflow: ellipsis;
+					max-width:98%;
+					overflow:hidden;
+				}
+				
+				.customNavigation{
+  					text-align: center;
+				}
+				.customNavigation i {
+					cursor: pointer;
+				}
+				.product-head {margin:10px auto;}
+				</style>
+				<?php $pc_mod_product_loaded = true;?>
+			<?php endif;?>
+		<?php }?>
+	<?php endif;?>
+<?php pc_mod_product_style();?>
 
 <script>
 (function($){
 	$(document).ready(function() {
-	 
+		 
 	$("#pc-mod-products-<?php echo $module->id;?>").owlCarousel({
 	 
 	autoPlay: false, //Set AutoPlay to 3 seconds
@@ -115,10 +128,19 @@ defined('_JEXEC') or die;
 	itemsDesktopSmall : [979,<?php echo $params->get('md_cols', 4);?>],
 	itemsTablet : [768,<?php echo $params->get('sm_cols', 3);?>],
 	itemsMobile : [400,<?php echo $params->get('xs_cols', 1);?>],
-	navigation : true,
-	pagination : false
+	navigation : false,
+	pagination : false,
+	navigationText : ["<",">"]
 	});
-	 
+	// Custom Navigation Events
+	$("#pc-module-products-<?php echo $module->id;?> .next").click(function(){
+		var owl = $("#pc-mod-products-<?php echo $module->id;?>").data('owlCarousel'); 
+		owl.next();
+	 	});
+	$("#pc-module-products-<?php echo $module->id;?> .prev").click(function(){
+		var owl = $("#pc-mod-products-<?php echo $module->id;?>").data('owlCarousel'); 
+		owl.prev();
+	});
 	});
 })(paycart.jQuery);
 
@@ -129,9 +151,16 @@ defined('_JEXEC') or die;
 <?php else:?>
 	<?php $ids = array_keys($products);?>
 <?php endif;?>
-
+<?php $formatter = PaycartFactory::getHelper('format');?>
 <div class="pc-module-product" id="pc-module-products-<?php echo $module->id;?>">
-	<div id="pc-mod-products-<?php echo $module->id;?>" class="pc-mod-products">
+	<div class="clearfix">
+		<h3 class="pull-left product-head"><?php echo $module->title;?></h3>						
+		<ul class="customNavigation pull-right list-inline">
+		  <li><i class="prev fa fa-angle-left fa-3x"></i></li>
+		  <li><i class="next fa fa-angle-right fa-3x"></i></li>
+		</ul>
+	</div>
+	<div id="pc-mod-products-<?php echo $module->id;?>" class="pc-mod-products">		
 		<?php foreach($ids as $id) : ?>
 			<?php $instance 	= PaycartProduct::getInstance($products[$id]->product_id);?>
 			<?php $media 		= $instance->getCoverMedia(true);?>
@@ -144,8 +173,8 @@ defined('_JEXEC') or die;
 					<span class="pc-mod-product-view">
 						<span class="btn btn-lg btn-primary">View</span>								
 					</span>
-					<h4 class="text-muted"><?php echo $products[$id]->title;?></h4>
-					<h4 class="text-muted"><?php echo $products[$id]->price;?></h4>
+					<h5 class="text-muted"><?php echo $products[$id]->title;?></h5>
+					<h4 ><?php echo $formatter->amount($products[$id]->price);?></h4>
 				</div>
 			</a>
 		<?php endforeach;?>				
