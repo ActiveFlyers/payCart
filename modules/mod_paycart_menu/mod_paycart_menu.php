3492c9106520471418a81c8b6bae84bf0e6856e5
rbsl-rimjhim <rimjhim@readybytes.in>
2015-02-26 11:12:41 +0530

    chore::move offcanvas css from site.css to paycart.css file

--- modules/mod_paycart_menu/mod_paycart_menu.php
+++ modules/mod_paycart_menu/mod_paycart_menu.php
@@ -24,6 +24,7 @@ include_once $file_path;
 Rb_HelperTemplate::loadMedia(array('jquery', 'bootstrap', 'rb', 'font-awesome'));
 Rb_HelperTemplate::loadSetupEnv();
 Rb_Html::script(PAYCART_PATH_CORE_MEDIA.'/paycart.js');
+Rb_Html::stylesheet(PAYCART_PATH_CORE_MEDIA.'/paycart.css');
 
 // get layout name
 $layout           = $params->get('layout', 'default');
