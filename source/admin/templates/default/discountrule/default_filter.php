99f0517b0b9405d6a363effb302f05282e419e74
rbsl-rimjhim <rimjhim@readybytes.in>
2015-03-30 12:23:19 +0530

    chore:: add date range filter on discount grid screen
    ref #676

--- source/admin/templates/default/discountrule/default_filter.php
+++ source/admin/templates/default/discountrule/default_filter.php
@@ -13,32 +13,38 @@ defined('_JEXEC') or die( 'Restricted access' );
 
 
 <div class="well">
-	<div class="row-fluid">	
-		<div class="span9">
-			<div class="row-fluid pc-filter-row">
-				<div class="pc-filter-minwidth-100 span2">
-					<label><?php echo JText::_('COM_PAYCART_ADMIN_TITLE')?></label>
-					<?php echo paycartHtml::_('paycarthtml.text.filter', 'title', 'discountrule', $filters, 'filter_paycart', array('class'=>'pc-filter-width'));?>
-				</div>
-				<div class="pc-filter-minwidth-100 span2">
-					<label><?php echo JText::_('COM_PAYCART_ADMIN_COUPON_CODE')?></label>
-					<?php echo paycartHtml::_('paycarthtml.text.filter', 'coupon', 'discountrule', $filters, 'filter_paycart', array('class'=>'pc-filter-width'));?>
-				</div>
-				<div class="pc-filter-minwidth-150 span2 pc-filter-gap-top">
-					<?php echo Rb_Html::_('rb_html.boolean.filter', 'published', 'discountrule', $filters, 'filter_paycart','COM_PAYCART_ADMIN');?>
-				</div>
-				<div class="pc-filter-minwidth-150 span2 pc-filter-gap-top">
-					<?php echo paycartHtml::_('paycarthtml.processor.filter','processor_classname','discountrule','discountrule',$filters,'filter_paycart');?>
-				</div>
-				<div class="pc-filter-minwidth-100 span2">
-					<label><?php echo JText::_('COM_PAYCART_ADMIN_AMOUNT')?></label>
-					<?php echo paycartHtml::_('paycarthtml.range.filter', 'amount', 'discountrule', $filters,'text','filter_paycart');?>
-				</div>
-				<div class="pc-filter-minwidth-50 span2 pc-filter-gap-top">
-					<div class="help-block"><input type="submit" name="filter_submit" class="btn-block btn btn-primary" value="<?php echo JText::_('COM_PAYCART_ADMIN_GO');?>" /></div>
-					<div><input type="reset"  name="filter_reset"  class="btn-block btn" value="<?php echo JText::_('COM_PAYCART_ADMIN_RESET');?>" onclick="paycart.admin.grid.filters.reset(this.form);" /></div>
-				</div>
-			</div>
+	<div class="row-fluid pc-filter-row">
+		<div class="pc-filter-minwidth-100 span1">
+			<label><?php echo JText::_('COM_PAYCART_ADMIN_TITLE')?></label>
+			<?php echo paycartHtml::_('paycarthtml.text.filter', 'title', 'discountrule', $filters, 'filter_paycart', array('class'=>'pc-filter-width'));?>
+		</div>
+		<div class="pc-filter-minwidth-100 span1">
+			<label><?php echo JText::_('COM_PAYCART_ADMIN_COUPON_CODE')?></label>
+			<?php echo paycartHtml::_('paycarthtml.text.filter', 'coupon', 'discountrule', $filters, 'filter_paycart', array('class'=>'pc-filter-width'));?>
+		</div>
+		<div class="pc-filter-minwidth-150 span1 pc-filter-gap-top">
+			<?php echo Rb_Html::_('rb_html.boolean.filter', 'published', 'discountrule', $filters, 'filter_paycart','COM_PAYCART_ADMIN');?>
+		</div>
+		<div class="pc-filter-minwidth-150 span2 pc-filter-gap-top">
+			<?php echo paycartHtml::_('paycarthtml.processor.filter','processor_classname','discountrule','discountrule',$filters,'filter_paycart');?>
+		</div>
+		<div class="pc-filter-minwidth-100 span2">
+			<label><?php echo JText::_('COM_PAYCART_ADMIN_AMOUNT')?></label>
+			<?php echo paycartHtml::_('paycarthtml.range.filter', 'amount', 'discountrule', $filters,'text','filter_paycart');?>
+		</div>
+		<div class="pc-filter-minwidth-150 span2 hidden-phone">
+			<label><?php echo JText::_('COM_PAYCART_ADMIN_START_DATE')?></label>
+			<?php echo paycartHtml::_('paycarthtml.range.filter', 'start_date', 'discountrule', $filters,'date','filter_paycart',array('style'=>'style="width:90px;"'));?>
+		</div>
+		
+		<div class="pc-filter-minwidth-150 span2 hidden-phone">
+			<label><?php echo JText::_('COM_PAYCART_ADMIN_END_DATE')?></label>
+			<?php echo paycartHtml::_('paycarthtml.range.filter', 'end_date', 'discountrule', $filters,'date','filter_paycart',array('style'=>'style="width:90px;"'));?>
+		</div>
+		
+		<div class="pc-filter-minwidth-50 span1 pc-filter-gap-top">
+			<div class="help-block"><input type="submit" name="filter_submit" class="btn-block btn btn-primary" value="<?php echo JText::_('COM_PAYCART_ADMIN_GO');?>" /></div>
+			<div><input type="reset"  name="filter_reset"  class="btn-block btn" value="<?php echo JText::_('COM_PAYCART_ADMIN_RESET');?>" onclick="paycart.admin.grid.filters.reset(this.form);" /></div>
 		</div>
 	</div>
 </div>
--- source/site/paycart/models/discountrule.php
+++ source/site/paycart/models/discountrule.php
@@ -26,6 +26,8 @@ class PaycartModelDiscountrule extends PaycartModelLang
 										'amount'	=> array('>=', '<='),
 										'processor_classname'=> array('='),
 										'published' => array('='),
+										'start_date'=>array('>=','<='),
+										'end_date'=>array('>=','<='),
 									);
 
 	
