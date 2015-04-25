66087789fbc929c54757713ddac8e268067a1329
Gaurav Jain <gaurav@readybytes.in>
2015-04-25 17:17:41 +0530

    fix :: Uses default weigth and dimension unit instead of hardcoding
    Other fix in javascript error handling in shippingrule screen.

--- source/admin/templates/default/shippingrule/default_edit.php
+++ source/admin/templates/default/shippingrule/default_edit.php
@@ -16,8 +16,7 @@ paycart.admin.shippingrule = {};
 (function($){
 	paycart.admin.shippingrule.getProcessorConfig = function(){
 		var processor_classname = $('[data-pc-shippingrule="processor"] select').val();
-		if(processor_classname.length > 0){
-			paycart.formvalidator.isValid('form.pc-form-validate');
+		if(processor_classname.length > 0){			
 			var url  = 'index.php?option=com_paycart&view=shippingrule&task=getProcessorConfig&processor_classname='+processor_classname+'&shippingrule_id='+<?php echo $form->getValue('shippingrule_id');?>;
 			paycart.ajax.go(url);
 		}
