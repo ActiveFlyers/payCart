cab0498bdc933793b56a3e32cb2171e6a8129e90
garima <garima@readybytes.in>
2015-05-15 19:13:26 +0530

    Feature::Add group rule or payplans plan with paycart

---
+++ plugins/plg_paycart_grouprulebuyerpayplans/grouprulebuyerpayplans.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+* @copyright        Copyright (C) 2009 - 2015 Ready Bytes Software Labs Pvt. Ltd. All rights reserved.
+* @license          GNU/GPL, see LICENSE.php
+* @package          PayartGrouprule.Buyer
+* @subpackage       BuyerPayplans
+* @contact          support+paycart@readybytes.in
+*/
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+/**
+*
+*/
+class PlgPaycartGroupruleBuyerpayplans extends RB_Plugin
+{
+	function __construct(& $subject, $config = array())
+	{
+    	parent::__construct($subject, $config);
+
+		//if payplans is not enabled/installed then do not load the plugin.
+    	if(!JComponentHelper::getComponent('com_payplans', true)->enabled){
+    		return true;
+    	}
+    	
+		 $helper = PaycartFactory::getHelper('group');		                      
+         $helper->push(Paycart::GROUPRULE_TYPE_BUYER, 'PaycartGroupruleBuyerPayplans',
+						array(
+							'filepath' 	=> dirname(__FILE__).'/rules/buyerpayplans/buyerpayplans.php',
+							'type'		=> paycart::GROUPRULE_TYPE_BUYER,
+							'title' 	=> 'PLG_PAYCART_GROUPRULEBUYERPAYPLANS_TITLE',
+							'description'=> 'PLG_PAYCART_GROUPRULEBUYERPAYPLANS_DESC',
+							'icon' 		=> ''
+                      ));
+	}
+}
---
+++ plugins/plg_paycart_grouprulebuyerpayplans/rules/buyerpayplans/tmpl/config.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+* @copyright        Copyright (C) 2009 - 2012 Ready Bytes Software Labs Pvt. Ltd. All rights reserved.
+* @license          GNU/GPL, see LICENSE.php
+* @package          PacartGrouprule.Buyer
+* @subpackage       BuyerPayplans
+* @contact          support+paycart@readybytes.in
+*/
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+?>
+<div class="control-group">
+	<label title="" class="hasTooltip control-label" for="paycart_form_title" id="paycart_form_title-lbl" 
+			data-original-title="&lt;strong&gt;<?php echo JText::_('Payplans Plan');?>&lt;/strong&gt;&lt;br /&gt;<?php echo JText::_('COM_PAYCART_ADMIN_DESCRIPTION');?>">
+		<?php echo JText::_('Payplans Plan');?>
+	</label>
+	<div class="controls">
+		<select class="paycart-grouprule-buyerpayplans-plan" name="<?php echo $namePrefix;?>[plan_assignment]" data-pc-selector="pc-option-manipulator" id="<?php echo $idPrefix;?>plan-assignment">
+			<option value="aboveAll" <?php echo !isset($config['plan_assignment']) || $config['plan_assignment'] == 'any' ? 'selected="selected"' : '';?>><?php echo Rb_Text::_('COM_PAYCART_ABOVE_ALL');?></option>
+			<option value="selected" <?php echo isset($config['plan_assignment']) && $config['plan_assignment'] == 'selected' ? 'selected="selected"' : '';?>><?php echo Rb_Text::_('COM_PAYCART_SELECTED');?></option>
+			<option value="except" <?php echo isset($config['plan_assignment']) && $config['plan_assignment'] == 'except' ? 'selected="selected"' : '';?>><?php echo Rb_Text::_('COM_PAYCART_EXCEPT');?></option>
+		</select>
+		
+		<span data-pc-option-manipulator="<?php echo $idPrefix.'plan-assignment';?>">
+		
+			<select class="paycart-grouprule-buyerpayplans-plan" name="<?php echo $namePrefix;?>[plan][]" multiple="true">
+				<?php foreach($userPlans as $plan_id => $data):?>
+					<option value="<?php echo $plan_id;?>" <?php echo isset($config['plan']) && in_array($plan_id, $config['plan']) ? 'selected="selected"' : '';?>><?php echo $data->title;?></option>
+				<?php endforeach;?>
+			</select>
+			
+		</span>
+	</div>
+</div>
