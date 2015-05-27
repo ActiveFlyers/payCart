4c676d18095b67172d42adbeeedbbed16a79b970
garima <garima@readybytes.in>
2015-05-27 15:08:56 +0530

    Inprovement::list all group rule plugin on grouprule screen

--- source/admin/templates/default/group/default_select_type.php
+++ source/admin/templates/default/group/default_select_type.php
@@ -11,11 +11,33 @@
 defined('_JEXEC') or die( 'Restricted access' );
 
 ?>
-<div>
-	<?php foreach($availableGroupRules as $rule_type => $rule) : ?>
-		<?php $rule = (object) $rule; ?>		
-		<a class="btn btn-large" href="index.php?option=com_paycart&view=group&task=new&type=<?php echo $rule_type;?>">
-			<?php echo JText::_('COM_PAYCART_ADMIN_GROUPRULE_TYPE_'.$rule_type);?>			
-		</a>		
-	<?php endforeach; ?>
-</div>
+
+<style>
+
+.paycart .table th, .table td {
+ text-align: center;
+}
+
+</style>
+	<div class="row-fluid">
+
+	<?php foreach($availableGroupRules as $rule_type => $rules) : ?>
+			<table class="table table-bordered table-responsive span4">
+	            <tr class="row-fluid">	
+					  <th  class="span12" ><b><?php echo ucfirst($rule_type)."  ".JText::_("COM_PAYCART_ADMIN_GROUP_RULES") ;?></b></th>
+				</tr> 
+		
+				<?php foreach ($rules as $key => $rule):?>
+		  				 <tr class="row-fluid">
+						  	  <td class="span12" >
+							  	  <a href="index.php?option=com_paycart&view=group&task=new&type=<?php echo $rule_type;?>">
+				 				  	<?php echo JText::_($rule->title);?>
+				 				  </a>
+			 				  </td>
+						  </tr>
+					<?php endforeach; ?>
+			</table>  
+   <?php endforeach; ?>
+	</div>	 
+
+<?php 
