3eb3e1671bed6e0e8df982d2e50a3af1cad06cae
rbsl-rimjhim <rimjhim@readybytes.in>
2015-06-03 11:00:06 +0530

    Fix::Allow to save notification data in multilingual on shipment failed
    Ref #749

--- source/admin/views/notification/view.ajax.php
+++ source/admin/views/notification/view.ajax.php
@@ -23,8 +23,17 @@ class PaycartAdminAjaxViewNotification extends PaycartAdminBaseViewNotification
             $itemid	= $this->getModel()->getId();
 
             $notification = PaycartNotification::getInstance($itemid);
+            
+            //PCTODO: Handle this case properly - Ticket #764
+            //There is an issue as we can add a new notification through sql file in english/default language only
+            //Required to set current language code, if trying to edit any notification in other langauge 
+            //and data is not available in that language
+            $data = $notification->toArray();
+            if($itemid && $data['lang_code'] != PaycartFactory::getPCCurrentLanguageCode()){
+            	$data['lang_code'] = PaycartFactory::getPCCurrentLanguageCode();
+            }
 
-            $form =  $notification->getModelform()->getForm();
+            $form =  $notification->getModelform()->getForm($data);
 
             $this->assign('form', $form );
             $this->assign('available_token_list', PaycartFactory::getHelper('token')->getTokenList($notification->getEventName()) );
