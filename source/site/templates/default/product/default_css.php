2d1f1a3fadc261f8aaa468fdc8e51dbb3c701cf7
garima <garima@readybytes.in>
2015-07-15 19:29:05 +0530

    Task::Add zoom image feature with inner and outer zoom configuration

---
+++ source/site/templates/default/product/default_css.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* @copyright	Copyright (C) 2009 - 2012 Ready Bytes Software Labs Pvt. Ltd. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* @package 		PAYCART
+* @subpackage	Front-end
+* @contact		support+paycart@readybytes.in
+*/
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+?>
+
+<style> 
+	.owl-theme .owl-controls .owl-buttons div {
+	  position: absolute;
+	}
+	 
+	.owl-theme .owl-controls .owl-buttons .owl-prev{
+	  left: 0px;
+	  top: 50%; 
+	}
+	 
+	.owl-theme .owl-controls .owl-buttons .owl-next{
+	  right: 0px;
+	  top: 50%;
+	}
+	
+	.fancybox-nav span {
+ 		visibility: visible;
+	}
+	
+	.pc-fancybox{
+	border: 1px solid #e5e5e5;
+    padding: 2px;
+    cursor: pointer;
+}
+</style>
