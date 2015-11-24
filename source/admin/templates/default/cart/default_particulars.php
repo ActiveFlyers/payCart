8e4ba70b6c08d3c61f2b94ab7273d48c70034923
rbsl-rimjhim <rimjhim@readybytes.in>
2015-11-24 11:31:28 +0530

    fix::make products linkable & show some attribute with cart in backend
    ref #960

--- source/admin/templates/default/cart/default_particulars.php
+++ source/admin/templates/default/cart/default_particulars.php
@@ -65,7 +65,24 @@ $currencyId = $cart->getCurrency();
 				<?php $discount += $particular->discount;?>
 				<?php $total    += $particular->total;?>
 				<tr>
-					<td><?php echo $particular->title; ?></td>
+					<td>
+						<?php echo PaycartHtml::link('index.php?option=com_paycart&view=product&task=edit&id='.$particular->particular_id, $particular->title); ?><br>
+						<?php $product = PaycartProduct::getInstance($particular->particular_id);?>
+ 						<?php $postionedAttributes = (array)$product->getPositionedAttributes();?>
+						<?php $attributes = $product->getAttributes();?>
+						 <div class="pc-product-overview">
+				 			<?php if(isset($postionedAttributes['product-overview']) && !empty($postionedAttributes['product-overview'])) : ?>
+								<ul class="unstyled muted">
+				 				<?php foreach($postionedAttributes['product-overview'] as $attributeId) : ?>
+				 					<?php if(isset($attributes[$attributeId]) && !empty($attributes[$attributeId])) :?>
+				 						<?php $instance = PaycartProductAttribute::getInstance($attributeId);?>
+							 			<li><small><?php echo $instance->getTitle();?>&nbsp;:&nbsp;<?php $options = $instance->getOptions(); echo $options[$attributes[$attributeId]]->title;?></small></li>
+									<?php endif?>
+				 				<?php endforeach;?>
+				 				</ul>
+				 			<?php endif;?>
+				 		</div>		
+					</td>
 					<td><?php echo $formatter->amount($particular->unit_price, true, $currencyId); ?></td>
 					<td><?php echo $particular->quantity; ?></td>
 					<td><?php echo $formatter->amount($particular->price, true, $currencyId); ?></td>
